bb39d91a9ec9805045ed8bc06b7fe16fb33898f8
Governor <latifabdulkarim11@gmail.com>
2024-06-29 11:58:51 +0000

    slight changes in the search bar

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import '../index.css'
 import { useState } from 'react';
 import { FaSearch } from 'react-icons/fa';
-import { useNavigate, useParams,  } from 'react-router-dom';
+import { useNavigate} from 'react-router-dom';
 
 
 
@@ -21,11 +21,11 @@ const Search = () => {
   return (
     <>
         <form className='flex items-center justify-center mb-[20px]' onSubmit={handleSubmit}>
-            <div className='d flex w-[50%]  items-center p-[5px] space-x-3 rounded-[10px]'>
-            <FaSearch className='text-white'/>
+            <div className='d flex w-[50%]  items-center py-[8px] px-[9px] space-x-3 rounded-[10px]'>
+            <FaSearch className='text-white text-sm'/>
                 <input type="text" name="search" id="search" value={search} onChange={(e)=> setSearch(e.target.value)}
-                  className='border-none outline-none  text-white w-[100%] bg-transparent'
-                  placeholder='search recipe..'
+                  className='border-none outline-none  text-white w-[100%] bg-transparent text-[13px]'
+                  placeholder='search recipe..' 
                 />
             
                 
